aaee98ddb21f332f6b76a778b88cabfc87e7481f
Amna-Alhammad <amnaq_1994@yahoo.com>
2021-02-07 14:54:44 +0200

    add lab 01

---
+++ code.js
@@ -0,0 +1,15 @@
+ let stname = prompt ('what is your name?');
+ alert('Welcome ' + stname  );
+ console.log ('your name is :' ,stname);
+
+let age = prompt ('How old are you?');
+alert(' Welcome  ' + stname  + ' and your age is : ' + age );
+console.log ('your age is :' ,age);
+
+let nationality = prompt ('what is your nationality?');
+alert( 'Welcome   ' + stname  + ' and you have : ' + age +'years old ' + 'your nationality is : ' + nationality   );
+console.log ('your nationality is :' ,nationality);
+
+let job = prompt ('what is your job');
+alert('Welcome ' + stname  + ' and you have : ' + age +' years old ' + 'your nationality is :  ' + nationality  +' your job is: ' + job );
+console.log ('your job is :' ,job);
